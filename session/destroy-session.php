a1d2262e0efeda1ccf646c7c60ddf9a52c38d5a7
santosh <santoshborate.mcs@gmail.com>
2018-01-04 23:59:44 +0530

    set session in cookie

---
+++ session/destroy-session.php
@@ -0,0 +1,22 @@
+<?php
+
+// Starting session
+
+session_start();
+
+
+
+// Removing session data
+
+if(isset($_SESSION["lastname"])){
+
+    unset($_SESSION["lastname"]);
+
+}
+
+// Destroying session
+
+session_destroy();
+
+?>
+
